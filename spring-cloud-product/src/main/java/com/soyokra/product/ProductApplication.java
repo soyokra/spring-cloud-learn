a7176957b87e952735610047b437ba97bddb1d4f
zhengchenping <zhengchenping@zuzuche.com>
2022-06-27 15:58:16 +0800

    +

--- spring-cloud-product/src/main/java/com/soyokra/product/ProductApplication.java
+++ spring-cloud-product/src/main/java/com/soyokra/product/ProductApplication.java
@@ -3,6 +3,9 @@ package com.soyokra.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class ProductApplication {
         SpringApplication.run(ProductApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate RestTemplate() {
+        return new RestTemplate();
+    }
 }
--- spring-cloud-product/src/main/java/com/soyokra/product/api/controller/ProductController.java
+++ spring-cloud-product/src/main/java/com/soyokra/product/api/controller/ProductController.java
@@ -3,17 +3,30 @@ package com.soyokra.product.api.controller;
 import com.soyokra.common.support.lib.StdResponse;
 import com.soyokra.product.api.controller.response.ProductResponse;
 import com.soyokra.product.domain.model.ProductModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping(value = "/api")
 @RestController
 public class ProductController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
     @GetMapping(value = "select-product")
     public @ResponseBody
     StdResponse<ProductResponse> getOrder() {
+
+        //根据应用名称调用服务
+        String json = restTemplate.getForObject("http://order/api/select-order", String.class);
+
         ProductModel productModel = new ProductModel();
         productModel.setProductId("bbbb");
         ProductResponse productResponse = new ProductResponse();
