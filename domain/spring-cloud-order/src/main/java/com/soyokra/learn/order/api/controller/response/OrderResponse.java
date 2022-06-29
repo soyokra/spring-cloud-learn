399e5a6332acd7982cbb9957ec7482adf73f2a58
zhengchenping <zhengchenping@zuzuche.com>
2022-06-29 14:10:07 +0800

    +

--- domain/spring-cloud-order/src/main/java/com/soyokra/learn/order/api/controller/response/OrderResponse.java
+++
@@ -1,9 +0,0 @@
-package com.soyokra.learn.order.api.controller.response;
-
-import com.soyokra.learn.order.domain.model.OrderModel;
-import lombok.Data;
-
-@Data
-public class OrderResponse {
-    private OrderModel order;
-}
