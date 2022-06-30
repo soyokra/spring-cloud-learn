bbf14f5d40f82892fb449a168103cb8a44f9ddb6
zhengchenping <zhengchenping@zuzuche.com>
2022-06-30 15:43:59 +0800

    +

--- spring-cloud-domain/spring-cloud-kernel/src/main/java/com/soyokra/learn/kernel/support/feign/OrderFeignContract.java
+++ spring-cloud-domain/spring-cloud-kernel/src/main/java/com/soyokra/learn/kernel/support/feign/OrderFeignContract.java
@@ -1,8 +1,15 @@
 package com.soyokra.learn.kernel.support.feign;
 
+import com.soyokra.learn.kernel.support.feign.response.order.OrderResponse;
+import com.soyokra.learn.kernel.support.utils.ResponseUtils;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Component
 @FeignClient("app-server-domain-order")
 public interface OrderFeignContract {
 
+    @GetMapping(value = "/api/select-order")
+    ResponseUtils<OrderResponse> getOrder();
 }
--- spring-cloud-domain/spring-cloud-kernel/src/main/java/com/soyokra/learn/kernel/support/feign/ProductFeignContract.java
+++ spring-cloud-domain/spring-cloud-kernel/src/main/java/com/soyokra/learn/kernel/support/feign/ProductFeignContract.java
@@ -1,7 +1,15 @@
 package com.soyokra.learn.kernel.support.feign;
 
+import com.soyokra.learn.kernel.support.feign.response.product.ProductResponse;
+import com.soyokra.learn.kernel.support.utils.ResponseUtils;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Component
 @FeignClient("app-server-domain-product")
 public interface ProductFeignContract {
+
+    @GetMapping(value = "/api/select-product")
+    ResponseUtils<ProductResponse> getProduct();
 }
